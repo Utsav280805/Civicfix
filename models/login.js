b5e4d5469c3cd9c63eff7c2c78cb92d056a366f2
23IT039-Lakshya <152374057+jivanilakshya@users.noreply.github.com>
2025-03-01 12:04:03 +0530

    Add files via upload

---
+++ models/login.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 4,
+    },
+    mobileNumber: {
+        type: String,
+        required: true,
+        unique: true,
+        
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
+
+
